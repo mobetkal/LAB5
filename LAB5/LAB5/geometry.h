4fd491d564e6891ed9d2c6db4b4a0d00cde5ab70
Marcin Obetkał <krecik.104@gmail.com>
2015-12-17 14:38:42 +0100

    #Zad1 #Zad2 #Zad3

---
+++ LAB5/LAB5/geometry.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class Geometry
+{
+public:
+	virtual float perimeter() const = 0;
+	virtual float area() const = 0;
+	virtual std::string toString() const = 0;
+
+	~Geometry() {};
+	Geometry() {};
+
+};
+
+class Triangle : public Geometry
+{
+	float a;
+public:
+	Triangle(float a) : a(a){};
+	Triangle(Triangle& T) { a = T.a; };
+	Triangle(Triangle&& T) { a = T.a; };
+	float perimeter() const
+	{
+		return 3 * a;
+	}
+
+	float area() const
+	{
+		return a*a*sqrt(3) / 4;
+	}
+	std::string toString() const
+	{
+		return "Trojkat o boku a=" + std::to_string(a) + " ma obwod: " + std::to_string(perimeter()) + " i pole: " + std::to_string(area());
+	}
+};
+
+class Circle : public Geometry
+{
+	float r;
+public:
+	Circle(float r) : r(r){};
+	Circle(Circle& T) { r = T.r; };
+	Circle(Circle&& T) { r = T.r; };
+	float perimeter() const
+	{
+		return 2 * 3.14*r;
+	}
+
+	float area() const
+	{
+		return 3.14*r*r;
+	}
+	std::string toString() const
+	{
+		return "Okrag o promieniu r=" + std::to_string(r) + " ma obwod: " + std::to_string(perimeter()) + " i pole: " + std::to_string(area());
+	}
+};
+
+class Rectangle : public Geometry
+{
+	float a;
+	float b;
+public:
+	Rectangle(float a, float b) : a(a), b(b){};
+	Rectangle(Rectangle& T) { a = T.a; b = T.b; };
+	Rectangle(Rectangle&& T) { a = T.a; b = T.b; };
+	float perimeter() const
+	{
+		return 2 * a + 2 * b;
+	}
+
+	float area() const
+	{
+		return a*b;
+	}
+	std::string toString() const
+	{
+		return "Prostokat o boku a=" + std::to_string(a) + " i b= " + std::to_string(b) + " ma obwod: " + std::to_string(perimeter()) + " i pole: " + std::to_string(area());
+	}
+};
--- LAB5/LAB5/main.cpp
+++ LAB5/LAB5/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "geometry.h"
 
 using namespace std;
 
 void main()
 {
-
-	cout << "Witam Kolege :))" << endl;
+	Triangle test1(10.0f);
+	Circle test2(10.0f);
+	Rectangle test3(10.0f, 10.0f);
+	cout << test1.toString() << endl;
+	cout << test2.toString() << endl;
+	cout << test3.toString() << endl;
 	system("PAUSE");
 }
